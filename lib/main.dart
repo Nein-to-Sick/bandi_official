ea00e918e9accbb2d627d81d6d15ebb0a1137df4
markFT39 <43339545+markFT39@users.noreply.github.com>
2024-07-04 11:10:51 +0900

    [Feat] Theme 임시 데이터 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,40 @@
+import 'package:bandi_official/theme/custom_theme_data.dart';
+import 'package:bandi_official/theme/custom_theme_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await dotenv.load(fileName: ".env");
+  CustomThemeMode.instance;
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: 'assets/config/.env');
 
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: CustomThemeMode.themeMode,
+      builder: (context, mode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: true,
+          darkTheme: CustomThemeData.dark,
+          theme: CustomThemeData.light,
+          themeMode: CustomThemeMode.themeMode.value,
+          home: const Placeholder(),
+        );
+      },
     );
   }
 }
