1b06392e7ada5cbad120523c51d638ebb430edc1
LocKey <rudfhr97@naver.com>
2024-07-22 00:41:42 +0900

    [Feat] Diary 작성 초기

--- lib/controller/navigation_toggle_provider.dart
+++ lib/controller/navigation_toggle_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NavigationToggleProvider with ChangeNotifier {
-  // _selectedIndex == 0 : home
-  // _selectedIndex == 1 : book
+  // _selectedIndex == 0 : 홈
+  // _selectedIndex == 1 : 일기
+  // _selectedIndex == 2 : 보관함
+  // _selectedIndex == 3 : 설정
   int _selectedIndex = 0;
 
   int get selectedIndex => _selectedIndex;
