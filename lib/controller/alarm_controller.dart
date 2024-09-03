8bf3f587d236e7475fa197e842c5c2cff65339a8
markFT39 <43339545+markFT39@users.noreply.github.com>
2024-09-03 13:25:52 +0900

    [Fix] 로컬 저장 함수 호출 수정
    코드 병합 후 중복된 함수 호출을 제거

--- lib/controller/alarm_controller.dart
+++ lib/controller/alarm_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-// TODO: 알람 보내기 이후 편지를 로컬에 저장하는 함수 호춣 필요
 class AlarmController with ChangeNotifier {
   // determine whether to display the alarm view
   bool isAlarmOpen = false;
