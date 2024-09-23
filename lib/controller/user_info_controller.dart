95a7fa136350c10637d6a1f8381ed4bf5608bdd5
sehan2709-cmis <sehan2020@gmail.com>
2024-09-23 16:10:14 +0900

    [Fix] 로그인 및 세팅관련 버그 수정

--- lib/controller/user_info_controller.dart
+++ lib/controller/user_info_controller.dart
@@ -4,7 +4,6 @@ class UserInfoValueModel with ChangeNotifier {
   String userId = '';
   String userEmail = '';
   String nickname = '';
-  bool isValueEntered = false;
 
   void updateUserEmail(String value) {
     userEmail = value;
@@ -16,16 +15,10 @@ class UserInfoValueModel with ChangeNotifier {
     notifyListeners();
   }
 
-  void setValueEntered() {
-    isValueEntered = true;
-    notifyListeners();
-  }
-
   void clearUserInfo() {
     userId = '';
     userEmail = '';
     nickname = '';
-    isValueEntered = false;
     notifyListeners();
   }
 }
--- lib/view/login/auth_service.dart
+++ lib/view/login/auth_service.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 import '../../controller/navigation_toggle_provider.dart';
 import '../../controller/user_info_controller.dart';
@@ -42,7 +43,7 @@ class AuthService {
         Provider.of<UserInfoValueModel>(context, listen: false);
     userInfoProvider.updateUserEmail(userEmail ?? '');
     userInfoProvider.userId = userId ?? '';
-    userInfoProvider.setValueEntered();
+    // userInfoProvider.setValueEntered();
 
     // NavigationToggleProvider를 사용하여 화면 전환
     final navigationToggleProvider =
@@ -86,5 +87,89 @@ class AuthService {
     }
 
     return FirebaseAuth.instance.currentUser;
+  }
+
+  Future signInWithApple(BuildContext context) async {
+    try {
+      // 1. 애플 로그인 자격 증명 가져오기
+      final AuthorizationCredentialAppleID appleCredential =
+          await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+
+      // 2. Firebase 자격 증명 생성
+      final OAuthProvider oAuthProvider = OAuthProvider("apple.com");
+      final AuthCredential credential = oAuthProvider.credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
+
+      // 3. Firebase에 로그인
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final userCollection = FirebaseFirestore.instance.collection("users");
+
+      String? userId = FirebaseAuth.instance.currentUser?.uid;
+      String? userEmail = FirebaseAuth.instance.currentUser?.email;
+
+      final docRef = userCollection.doc(userId);
+      DocumentSnapshot snapshot = await docRef.get();
+
+      final userInfoProvider =
+          Provider.of<UserInfoValueModel>(context, listen: false);
+      userInfoProvider.updateUserEmail(userEmail ?? '');
+      userInfoProvider.userId = userId ?? '';
+      // userInfoProvider.setValueEntered();
+
+      // NavigationToggleProvider를 사용하여 화면 전환
+      final navigationToggleProvider =
+          Provider.of<NavigationToggleProvider>(context, listen: false);
+
+      if (!snapshot.exists) {
+        // 처음 로그인한 사용자라면
+        await docRef.set({
+          "created_at": FieldValue.serverTimestamp(),
+          "email": userEmail,
+          "nickname": "",
+          "likedDiaryId": [],
+          "myDiaryId": [],
+          "socialLoginProvider": "apple", // 구글 대신 애플로 설정
+          "updatedAt": FieldValue.serverTimestamp(),
+          "userId": userId,
+          "newLetterAvailable": false,
+        });
+
+        // 하위 컬렉션 생성: letters
+        final lettersCollectionRef = docRef.collection('letters');
+
+        // 하위 컬렉션: letters에 대표 문서 추가
+        await lettersCollectionRef.doc('0000_docSummary').set({
+          // 빈 데이터 또는 초기 데이터
+        });
+
+        // 하위 컬렉션 생성: otherDiary
+        final otherDiaryCollectionRef = docRef.collection('otherDiary');
+
+        // 하위 컬렉션: otherDiary에 빈 문서 추가
+        await otherDiaryCollectionRef.doc('0000_docSummary').set({
+          // 빈 데이터 또는 초기 데이터
+        });
+
+        // 온보딩 페이지로 이동
+        navigationToggleProvider.selectIndex(-3);
+      } else {
+        // 기존 사용자라면 바로 홈 페이지로 이동
+        navigationToggleProvider.selectIndex(0);
+      }
+
+      return FirebaseAuth.instance.currentUser;
+    } catch (error) {
+      print('애플 로그인 중 오류 발생: $error');
+      return null;
+    }
   }
 }
