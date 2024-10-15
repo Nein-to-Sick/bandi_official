b2fd379840a6188e63a837aa821b21b4f21ba8f2
sehan2709-cmis <sehan2020@gmail.com>
2024-10-15 14:34:15 +0900

    [Chore] few settings updates

--- lib/view/navigation.dart
+++ lib/view/navigation.dart
@@ -122,7 +122,8 @@ class _NavigationState extends State<Navigation> with WidgetsBindingObserver {
                   ? OtherDiary(
                       writeProvider: writeProvider,
                     )
-                  : navigationToggleProvider.selectedIndex == -1
+                  : (navigationToggleProvider.selectedIndex <= -1 &&
+                          navigationToggleProvider.selectedIndex != -2)
                       ? const LoginView()
                       : navigationToggleProvider.selectedIndex == 0
                           ? const HomePage()
