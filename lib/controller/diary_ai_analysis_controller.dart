42820b752db39581dd3f48506d2eb4d6bf570615
HyungJin Kim <43339545+markFT39@users.noreply.github.com>
2025-05-22 20:58:11 +0900

    [Fix] 일기 분석 프롬프트 수정

--- lib/controller/diary_ai_analysis_controller.dart
+++ lib/controller/diary_ai_analysis_controller.dart
@@ -85,7 +85,7 @@ class DiaryAIAnalysisController with ChangeNotifier {
         content: [
           OpenAIChatCompletionChoiceMessageContentItemModel.text(
             (langCode == 'ko')
-                ? "일기 내용을 분석하고 한 문장의 제목으로 요약해줘."
+                ? "일기 내용을 분석하고 한 문장의 한국어 제목으로 요약해줘."
                 : "Given a diary entry, analyze the text and summarize it with a title in English.",
           ),
         ],
@@ -151,8 +151,8 @@ class DiaryAIAnalysisController with ChangeNotifier {
         content: [
           OpenAIChatCompletionChoiceMessageContentItemModel.text(
             (langCode == 'ko')
-                ? "일기 내용에 기반하여 하나의 문장으로 격려를 한국어로 제공해줘."
-                : "Please limit your response to a single sentence and provide encouragement in English based on the content of the diary entry.",
+                ? "일기 내용에 기반하여 하나의 한국어 문장으로 응원을 작성해줘."
+                : "Please limit your response to a simple single sentence and provide encouragement in English based on the content of the diary entry.",
           ),
         ],
         role: OpenAIChatMessageRole.system,
