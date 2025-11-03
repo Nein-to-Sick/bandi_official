9b1fb5847acda95a5824dfe50b60842bf846d297
HyungJin Kim <43339545+markFT39@users.noreply.github.com>
2025-11-03 21:55:36 +0900

    [Fix] 불필요한 log 제거

--- lib/analytics/log_ai_chat_send.dart
+++ lib/analytics/log_ai_chat_send.dart
@@ -1,12 +1,9 @@
-import 'dart:developer';
-
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 final analytics = FirebaseAnalytics.instance;
 
 Future<void> logAIChatSend(
     {required bool chatSend, required int chatLength}) async {
-  log('$chatSend and $chatLength');
   await FirebaseAnalytics.instance.logEvent(
     name: 'ai_chat_send',
     parameters: {
