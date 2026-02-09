582b895dd261d75cad0182190b5c32cbf2856fd6
LocKey <rudfhr97@naver.com>
2026-02-09 21:57:28 +0900

    fix: 더블 클릭 위험 제거

--- lib/view/login/data/user_profile_repository.dart
+++ lib/view/login/data/user_profile_repository.dart
@@ -111,7 +111,7 @@ class UserProfileRepository {
     required String userId,
     required bool isAgreed,
   }) async {
-    await _users.doc(userId).set({
+    await _users.doc(userId).update({
       'isAgreed': isAgreed,
       'last_agreed_at': isAgreed ? DateTime.now() : null,
       'updatedAt': FieldValue.serverTimestamp(),
